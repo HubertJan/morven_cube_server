31876ab96ab49e44ef701b060a41db79dbb85f7a
HubertJan <70658889+HubertJan@users.noreply.github.com>
2022-09-06 12:08:55 +0200

    fix

--- morven_cube_server/zauber.py
+++ morven_cube_server/zauber.py
@@ -7,16 +7,19 @@ from morven_cube_server.services.primary_service import PrimaryService
 from morven_cube_server.services.secondary_service import SecondaryService
 from morven_cube_server.states.primary_arduino_state import PrimaryServiceState
 from morven_cube_server.states.server_state import ServerState
-from morven_cube_server.services.rubiks_database_service import RubiksDatabaseService
 from morven_cube_server.state_handler.background_task import add_background_task
 from morven_cube_server.state_handler.provider import provide
+from morven_cube_server.background_tasks.handle_primary_updates import handle_primary_updates
 
 from morven_cube_server.services.primary_service_state import PrimaryArduinoService
 from morven_cube_server.services.secondary_arduino_service import SecondaryArduinoService
+from morven_cube_server.models.primary_arduino_status import PrimaryArduinoStatus
+
 from morven_cube_server.routes import *
 
 from morven_cube_server.dummies.dummy_primary_arduino_service import DummyPrimaryArduinoService
 from morven_cube_server.dummies.dummy_secondary_arduino_service import DummySecondaryArduinoService
+
 
 
 def run_server() -> None:
@@ -41,10 +44,6 @@ def run_server() -> None:
     ), valueType=ServerState)
     provide(app=app, value=PrimaryServiceState(),
             valueType=PrimaryServiceState)
-    provide(app=app,
-            value=RubiksDatabaseService(database_file_name="db_cube.csv"),
-            valueType=RubiksDatabaseService
-            )
 
     add_background_task(
         app=app,
@@ -57,8 +56,7 @@ def run_server() -> None:
 
     web.run_app(app, host='localhost', port=9000)
 
-
-def run_test_server() -> None:
+def run_test_server()-> None:
     app = web.Application()
     app.add_routes(routes=routes)
 
@@ -80,12 +78,9 @@ def run_test_server() -> None:
             max_speed=50,
         )
     ), valueType=ServerState)
-    provide(app=app, value=PrimaryServiceState(),
-            valueType=PrimaryServiceState)
     provide(app=app,
-            value=RubiksDatabaseService(database_file_name="db_cube.csv"),
-            valueType=RubiksDatabaseService
-            )
+            value=PrimaryServiceState(),
+            valueType=PrimaryServiceState)
 
     add_background_task(
         app=app,
@@ -95,5 +90,9 @@ def run_test_server() -> None:
         app=app,
         task_func=update_state_with_current_sensor_data,
     )
+    add_background_task(
+        app=app,
+        task_func=handle_primary_updates
+    )
 
-    web.run_app(app, host='localhost', port=9000)
+    web.run_app(app, host='192.168.0.42', port=9000)
